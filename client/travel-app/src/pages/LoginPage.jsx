56d4c62ff805ac7955dc5341f9020b907828432c
machleslie <machariales@gmail.com>
2024-02-28 16:07:43 +0300

    Revert "chore("
    This reverts commit f82272e2010fd8c75605b48e5ac0f03e6458b4bd.

--- client/travel-app/src/pages/LoginPage.jsx
+++ client/travel-app/src/pages/LoginPage.jsx
@@ -1,105 +1,36 @@
-import React, { useState } from 'react';
-import { useNavigate } from 'react-router-dom'; // Import useNavigate
-import Swal from 'sweetalert2';
-import loginImage from '../images/homepage.jpg';
+import React from 'react';
+import loginImage from '../images/homepage.jpg'; // Path to your login image
 
 const LoginPage = () => {
-  const navigate = useNavigate(); // Initialize useNavigate
-  const [formData, setFormData] = useState({
-    email: '',
-    password: '',
-    loginAsAdmin: false,
-  });
-
-  const handleInputChange = (e) => {
-    const { name, value, type, checked } = e.target;
-
-    if (type === 'checkbox') {
-      setFormData((prevData) => ({
-        ...prevData,
-        loginAsAdmin: name === 'login-as-admin' ? checked : false,
-      }));
-    } else {
-      setFormData((prevData) => ({
-        ...prevData,
-        [name]: value,
-      }));
-    }
-  };
-
-  const handleFormSubmit = async (e) => {
-    e.preventDefault();
-
-    const endpoint = formData.loginAsAdmin ? '/admin_login' : '/login';
-
-    try {
-      const response = await fetch(endpoint, {
-        method: 'POST',
-        headers: {
-          'Content-Type': 'application/json',
-        },
-        body: JSON.stringify(formData),
-      });
-
-      if (response.ok) {
-        // Successful login, show success message
-        Swal.fire({
-          icon: 'success',
-          title: 'Login Successful',
-          text: 'You have successfully logged in.',
-        }).then(() => {
-          // Redirect to the home page
-          navigate('/');
-        });
-      } else {
-        Swal.fire({
-          icon: 'error',
-          title: 'Login Failed',
-          text: 'Invalid email or password. Please try again.',
-        });
-      }
-    } catch (error) {
-      Swal.fire({
-        icon: 'error',
-        title: 'Error',
-        text: 'An error occurred during login. Please try again later.',
-      });
-    }
-  };
-
   return (
-    <div className="flex justify-center items-center h-screen" style={{ backgroundImage: `url(${loginImage})`, backgroundSize: 'cover', backgroundPosition: 'center' }}>
+    <div
+      className="flex justify-center items-center h-screen"
+      style={{ backgroundImage: `url(${loginImage})`, backgroundSize: 'cover', backgroundPosition: 'center'}}
+    >
       <div className="container mx-auto max-w-md p-8 bg-white bg-opacity-75 rounded-lg shadow-md">
         <h2 className="text-2xl font-bold mb-4">Login</h2>
-        <form className="space-y-4" onSubmit={handleFormSubmit}>
+        <form className="space-y-4">
           <div>
-            <label htmlFor="email" className="block text-gray-700">
-              Email:
-            </label>
-            <input type="email" id="email" name="email" className="w-full border rounded p-2 mt-1" onChange={handleInputChange} />
+            <label htmlFor="username" className="block text-gray-700">Username :</label>
+            <input type="text" id="username" name="username" className="w-full border rounded p-2 mt-1" />
           </div>
           <div>
-            <label htmlFor="password" className="block text-gray-700">
-              Password:
-            </label>
-            <input type="password" id="password" name="password" className="w-full border rounded p-2 mt-1" onChange={handleInputChange} />
+            <label htmlFor="email" className="block text-gray-700">Email :</label>
+            <input type="email" id="email" name="email" className="w-full border rounded p-2 mt-1" />
+          </div>
+          <div>
+            <label htmlFor="password" className="block text-gray-700">Password :</label>
+            <input type="password" id="password" name="password" className="w-full border rounded p-2 mt-1" />
           </div>
           <div className="flex items-center">
-            <input
-              type="checkbox"
-              id="login-as-admin"
-              name="login-as-admin"
-              className="mr-2"
-              checked={formData.loginAsAdmin}
-              onChange={handleInputChange}
-            />
-            <label htmlFor="login-as-admin" className="text-gray-700">
-              Login as admin
-            </label>
+            <input type="checkbox" id="login-as-user" name="login-as-user" className="mr-2" />
+            <label htmlFor="login-as-user" className="text-gray-700">Login as user</label>
           </div>
-          <button type="submit" className="bg-orange-500 text-white py-3 px-6 rounded">
-            Login
-          </button>
+          <div className="flex items-center">
+            <input type="checkbox" id="login-as-company" name="login-as-company" className="mr-2" />
+            <label htmlFor="login-as-company" className="text-gray-700">Login as company</label>
+          </div>
+          <button type="submit" className="bg-orange-500 text-white py-3 px-6 rounded">Login</button>
         </form>
       </div>
     </div>
